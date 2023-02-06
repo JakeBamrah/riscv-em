7dbe8ec11ca94be3978578e81786d88e2d3123fe
Jake Bamrah <login@bamrah.sh>
2023-02-06 23:30:11 +0000

    add cpu instruction decode functions

--- cpu.c
+++ cpu.c
@@ -24,3 +24,55 @@ uint32_t cpu_fetch(CPU *cpu) {
     uint32_t inst = cpu_load(cpu, cpu->program_counter, 32);
     return inst;
 }
+
+uint64_t cpu_decode_opcode(uint32_t inst) {
+    return inst & 0x7f;
+}
+
+uint64_t cpu_decode_rd(uint32_t inst) {
+    return (inst >> 7) & 0x1f;
+}
+
+uint64_t cpu_decode_rs1(uint32_t inst) {
+    return (inst >> 15) & 0x1f;
+}
+
+uint64_t cpu_decode_rs2(uint32_t inst) {
+    return (inst >> 20) & 0x1f;
+}
+
+uint64_t cpu_decode_imm_I(uint32_t inst) {
+    /* bits[11:0] of inst[31:20] */
+    return inst >> 20;
+}
+
+uint64_t cpu_decode_imm_S(uint32_t inst) {
+    /* inst[31:25]      | bits [4:0] of inst[11:7] */
+    return (inst >> 25) | ((inst >> 7) & 0x1f);
+}
+
+uint64_t cpu_decode_imm_B(uint32_t inst) {
+    /* imm[12|10:5|4:1|11] = inst[31|30:25|11:8|7] */
+    return ((inst & 0x80000000) >> 19)
+        | ((inst & 0x80) << 4) // imm[11]
+        | ((inst >> 20) & 0x7e0) // imm[10:5]
+        | ((inst >> 7) & 0x1e); // imm[4:1]
+}
+
+uint64_t cpu_decode_imm_U(uint32_t inst) {
+    /* inst[31:25]      | bits [4:0] of inst[11:7] */
+    return inst & 0xfffff999;
+}
+
+uint64_t cpu_decode_imm_J(uint32_t inst) {
+    /* imm[20|10:1|11|19:12] = inst[31|30:21|20|19:12] */
+    return ((inst & 0x80000000) >> 11)
+        | (inst & 0xff000) // imm[19:12]
+        | ((inst >> 9) & 0x800) // imm[11]
+        | ((inst >> 20) & 0x7fe); // imm[10:1]
+}
+
+uint64_t cpu_decode_shamt(uint32_t inst) {
+    /* bits[5:0] of inst[31:20] */
+    return cpu_decode_imm_I(inst) & 0x1f;
+}
--- risc.h
+++ risc.h
@@ -51,3 +51,45 @@ uint32_t cpu_fetch(CPU *cpu);
 
 /* A basic ALU and decoder—decodes a fetched instruction and executes it */
 int cpu_execute(CPU *cpu, uint32_t inst);
+
+
+/*
+------ CPU INSTRUCTION DECODING -------
+
+R-Type: Register type instructions
+I-Type: Immediate type instructions
+S-Type: Store type instructions
+B-Type: Break type instructions
+U-Type: Register type instructions
+J-Type: Jump type instructions
+*/
+
+/* Returns opcode register—7-bit address of destination register [6:0] */
+uint64_t cpu_decode_opcode(uint32_t inst);
+
+/* Returns rd register—5-bit address of destination register [11:7] */
+uint64_t cpu_decode_rd(uint32_t inst);
+
+/* Returns rs1 register—4-bit address of destination register [19:15] */
+uint64_t cpu_decode_rs1(uint32_t inst);
+
+/* Returns rs2 register—4-bit address of destination register [24:20] */
+uint64_t cpu_decode_rs2(uint32_t inst);
+
+/* bits[11:0] of inst[31:20] */
+uint64_t cpu_decode_imm_I(uint32_t inst);
+
+/* bits [11:5] of inst[31:25], bits [4:0] of inst[11:7] */
+uint64_t cpu_decode_imm_S(uint32_t inst);
+
+/* imm[12|10:5|4:1|11] of inst[31|30:25|11:8|7] */
+uint64_t cpu_decode_imm_B(uint32_t inst);
+
+/* bits [31:12] */
+uint64_t cpu_decode_imm_U(uint32_t inst);
+
+/* imm[20|10:1|11|19:12] of inst[31|30:21|20|19:12] */
+uint64_t cpu_decode_imm_J(uint32_t inst);
+
+/* Returns shamt register—5-bit address of destination register [5:0] */
+uint64_t cpu_decode_shamt(uint32_t inst);
