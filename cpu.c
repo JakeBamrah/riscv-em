48bb63b580c879088456fa4c6f879a35a053caea
Jake Bamrah <login@bamrah.sh>
2023-02-11 22:54:35 +0000

    preserve immediate signs before casting to unsigned 64-bit

--- cpu.c
+++ cpu.c
@@ -19,8 +19,8 @@ void cpu_store(CPU *cpu, uint64_t addr, uint64_t size, uint64_t value) {
 }
 
 uint32_t cpu_fetch(CPU *cpu) {
-    // fetch next instruction to be executed by loading data onto bus from dram
-    // at the specified address given by program-counter
+    /* fetch next instruction to be executed by loading data onto bus from dram
+       at the specified address given by program-counter */
     uint32_t inst = cpu_load(cpu, cpu->program_counter, 32);
     return inst;
 }
@@ -46,18 +46,20 @@ uint64_t cpu_decode_rs2(uint32_t inst) {
 }
 
 uint64_t cpu_decode_imm_I(uint32_t inst) {
-    /* imm[11:0] = inst[31:20] */
-    return inst >> 20;
+    /* imm[11:0] = inst[31:20]
+       immediates are signed integers (bit 31 contains sign)-preserve signed int
+       before casting to 64-bit unsigned */
+    return (int64_t)(int32_t)inst >> 20;
 }
 
 uint64_t cpu_decode_imm_S(uint32_t inst) {
     /* imm[11:5] = inst[31:25], imm[4:0] = inst[11:7] */
-    return (inst >> 25) | ((inst >> 7) & 0x1f);
+    return ((int64_t)(int32_t)inst >> 25) | ((inst >> 7) & 0x1f);
 }
 
 uint64_t cpu_decode_imm_B(uint32_t inst) {
     /* imm[12|10:5|4:1|11] = inst[31|30:25|11:8|7] */
-    return ((inst & 0x80000000) >> 19)
+    return ((int64_t)(int32_t)(inst & 0x80000000) >> 19)
         | ((inst & 0x80) << 4) // imm[11]
         | ((inst >> 20) & 0x7e0) // imm[10:5]
         | ((inst >> 7) & 0x1e); // imm[4:1]
@@ -65,12 +67,12 @@ uint64_t cpu_decode_imm_B(uint32_t inst) {
 
 uint64_t cpu_decode_imm_U(uint32_t inst) {
     /* imm[31:12] = inst[31:12] */
-    return inst & 0xfffff999;
+    return (int64_t)(int32_t)(inst & 0xfffff999);
 }
 
 uint64_t cpu_decode_imm_J(uint32_t inst) {
     /* imm[20|10:1|11|19:12] = inst[31|30:21|20|19:12] */
-    return ((inst & 0x80000000) >> 11)
+    return ((int64_t)(int32_t)(inst & 0x80000000) >> 11)
         | (inst & 0xff000) // imm[19:12]
         | ((inst >> 9) & 0x800) // imm[11]
         | ((inst >> 20) & 0x7fe); // imm[10:1]
